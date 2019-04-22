4b44e768e05ddcbf39526fddb7a18c92a77c3d32
hirame <48191340+tekkui@users.noreply.github.com>
2019-04-22 16:30:29 +0900

    Initial commit

---
+++ 7zFontChange/7zFontChange.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <fstream>
+#include <conio.h>
+
+int main()
+{
+	char targetFiles[][10] = { {"7zFM.exe"}, {"7zG.exe"}, {"7z.sfx"}, {""} };
+
+	char orgFont[] = { 0x08, 0x00, 'M', 0x00, 'S', 0x00, ' ', 0x00, 'S', 0x00, 'h', 0x00, 'e', 0x00, 'l', 0x00, 'l', 0x00, ' ', 0x00, 'D', 0x00, 'l', 0x00, 'g' };
+	char newFont[] = { 0x0a, 0x00, 'Y', 0x00, 'u', 0x00, ' ', 0x00, 'G', 0x00, 'o', 0x00, 't', 0x00, 'h', 0x00, 'i', 0x00, 'c', 0x00, ' ', 0x00, 'U', 0x00, 'I' };
+
+	for (int t = 0; targetFiles[t][0] != NULL; t++)
+	{
+		std::ifstream ifs;
+		ifs.open(targetFiles[t], std::ios::in | std::ios::binary);
+
+		if (!ifs)
+		{
+			std::cout << targetFiles[t] << " not found" << std::endl;
+			continue;
+		}
+
+		ifs.seekg(0, std::fstream::end);
+		std::streamoff eofPos = ifs.tellg();
+		ifs.clear();
+		ifs.seekg(0, std::fstream::beg);
+		std::streamoff begPos = ifs.tellg();
+		int size = (int)(eofPos - begPos);
+
+		char *buf = new char[size];
+		memset(buf, 0, size);
+		ifs.read(buf, size);
+		ifs.close();
+
+		int oSize = sizeof(orgFont);
+		int nSize = sizeof(newFont);
+
+		std::cout << "Search from " << targetFiles[t] << std::endl;
+		bool changed = false;
+		for (int i = 0; i < size; i++)
+		{
+			if (buf[i] == orgFont[0])
+			{
+				for (int j = 1; j < oSize; j++)
+				{
+					if (buf[i + j] != orgFont[j])
+					{
+						break;
+					}
+					else
+					{
+						if (j == oSize - 1)
+						{
+							std::cout << "Found at 0x" << std::hex << i << std::endl;
+							changed = true;
+							for (j = 0; j < nSize; j++)
+								buf[i + j] = newFont[j];
+							break;
+						}
+					}
+				}
+
+			}
+		}
+
+		if (changed)
+		{
+			std::string newName(targetFiles[t]);
+			newName += ".Original";
+			std::rename(targetFiles[t], newName.c_str());
+			std::cout << "Change to " << newName.c_str() << std::endl;
+
+			std::ofstream ofs;
+			ofs.open(targetFiles[t], std::ios::out | std::ios::trunc | std::ios::binary);
+			ofs.write(buf, size);
+			ofs.close();
+			std::cout << "Rewritten and saved" << std::endl;
+		}
+
+		delete[] buf;
+	}
+
+	std::cout << "Press any key to continue ..." << std::endl;
+	_getch();
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# 7zFontChange
+[![CodeFactor](https://www.codefactor.io/repository/github/tekkui/7zFontChange/badge)](https://www.codefactor.io/repository/github/tekkui/7zFontChange)
+[![Build status](https://ci.appveyor.com/api/projects/status/28xirs01hf1xgq15/branch/master?svg=true)](https://ci.appveyor.com/project/tekkui/7zFontChange/branch/master)
+[![License: MIT](https://img.shields.io/badge/License-MIT-lightgrey.svg)](https://opensource.org/licenses/MIT)
+[![Github Releases All](https://img.shields.io/github/downloads/tekkui/7zFontChange/total.svg)](https://github.com/tekkui/7zFontChange/releases "All Releases")
+
+## 機能
+7-Zip File Manager、GUI、SFXのダイアログに設定されているフォントを書き換える。
+"8pt MS Shell Dlg" から "10pt Yu Gothic UI"に変更する。
+
+## 使い方
+7-Zipをインストールしたフォルダに7zFontChange.exeをコピーして、管理者として実行する。
+
+## CI
+- [AppVeyor](https://ci.appveyor.com/project/tekkui/7zFontChange/branch/master)
+
+## Code Review
+- [CodeFactor](https://www.codefactor.io/repository/github/tekkui/7zFontChange)
+
+## 開発
+### 環境
+- [Visual Studio Community 2017](https://www.visualstudio.com/)
+
+### 詳細
+UnifyZip.slnをVisual Studioで開いてビルドする。
